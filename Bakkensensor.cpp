202ddc5d81686d125a9006276d119f9fddc3fee4
DeHanSolo <101405318+DeHanSolo@users.noreply.github.com>
2022-03-17 12:01:57 +0100

    Update Bakkensensor.cpp

--- Bakkensensor.cpp
+++ Bakkensensor.cpp
@@ -1,16 +1,13 @@
-#include "bakkensensor.h"
+#include "Bakkensensor.h"
       
-Bakkensenosor(){
+Bakkensensor(){
     int max = int_min;
     int max_i = 0;
-
 
       for(int i = 0; i < 4; i++) {
         if( pinIrBakken[i] > max){
           max = pinIrBakken[i];
           Max_i = i;
-          
-          
         }
-  }
+      }
 }
