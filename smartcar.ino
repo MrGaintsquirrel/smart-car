462d9f8d0ff6d5d5d2bbac8b6d3721faa8fce6ea
thijsvanbovene <39094133+ThijsvanBovene@users.noreply.github.com>
2022-03-19 18:08:16 +0100

    car drives to target

--- smartcar.ino
+++ smartcar.ino
@@ -9,6 +9,16 @@ ultrasonic ultrasonic(A0, A1, 10);
 
 car smartcar;
 
+unsigned long Time = 0;
+unsigned long previousTime = 0;
+
+enum state {
+  searching = 0,
+  found = 1,
+} _State;
+
+int Stopflag = 0;
+
 void setup() {
   // put your setup code here, to run once:
   
@@ -25,8 +35,41 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(ultrasonic.getDistance());
+  //Serial.println(ultrasonic.getDistance());
 
+  Time = millis();
+  int sensorid = 5;
+
+  if(Time - previousTime >= 50){
+    if(Bakkensensor.getSensorValue(Bakkensensor.gethighestsensor()) > 15) { // Check if sensor is above threshold to make sure it is the target
+      sensorid = Bakkensensor.gethighestsensor();
+    }
+  }
+
+  if(sensorid == 0){ // Drive forwards if the target is in front
+     _State = found;
+     if(Stopflag == 0){
+      smartcar.driveforward(50);
+     }
+     if(Time - previousTime >= 2000 && _State == found){
+      Stopflag = 1;
+      smartcar.Stop();
+     }
+   } else{
+    _State = searching;
+   }
+   
+   if(_State == searching && Time - previousTime <= 2000) {
+     smartcar.driveturnright(40);
+   }
+   if(_State == searching && Time - previousTime >= 2000) {
+      smartcar.driveleft(50);
+      if(Time - previousTime >= 4000){
+        previousTime = Time;
+    }
+   }
+
+/*
   smartcar.driveforward(50);
   delay(5000);
   smartcar.drivebackward(50);
@@ -39,5 +82,5 @@ void loop() {
   delay(5000);
   smartcar.driveturnleft(50);
   delay(5000);
-  
+  */
 }
